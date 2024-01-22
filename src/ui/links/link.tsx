fdd071cd9b6c7cd8c07425a70e0c7b7766709929
NataliyaNikulshina <victory515@yandex.ru>
2024-01-22 17:12:18 +0300

    feat: добавить новый тип ссылки

--- src/ui/links/link.tsx
+++ src/ui/links/link.tsx
@@ -1,23 +1,29 @@
+import { BinaryToTextEncoding } from "crypto";
 import React, { FC } from "react";
 
 import stylesILink from "./link.module.scss";
 
 export interface ILink {
   size?: string;
+  weight?: string;
   color?: "black" | "blue";
-  href?: string;
-  children?: React.ReactNode;
+  underline?: boolean;
+  href: string;
+  children: React.ReactNode;
 }
 
 const Link: FC<ILink> = ({
-    children, href, size = 18, color='blue'
+    children, href, size = '18', weight = '400', color='blue', underline=true
 }) => {
+
+  const styleColor = color==='blue' ? stylesILink.blue : stylesILink.black;
+  const styleAll = !underline ? styleColor + " " + stylesILink.underline : styleColor;
   
   return (
     <a
     href={href}
-    className={color==='blue' ? stylesILink.blue : stylesILink.black}
-    style={{ fontSize: size + "ph" }}
+    className={styleAll}
+    style={{ fontSize: `${size}px`, fontWeight: weight }}
     >
       {children}
     </a>
