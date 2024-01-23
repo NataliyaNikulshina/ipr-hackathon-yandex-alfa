13758ea6988cb03143d9736aa6325e5268362a75
NataliyaNikulshina <victory515@yandex.ru>
2024-01-23 16:51:51 +0300

    feat: Создать компонент карточки рук-ля и сотр-ка

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -2,6 +2,7 @@ import { FC, useState, useEffect, FormEvent } from "react";
 import stylesProfile from "./homePage.module.scss";
 
 import Statistics from "../../components/Statistics/Statistics";
+import Card from "../../components/card/card";
 
 //  Ссылки на картинки инфо попапа
 import darts from "./images/darts.png";
@@ -10,11 +11,24 @@ import dartsTask from "./images/darts-task.png";
 //import  { handlePopup }  from "../../app";
 
 const HomePage: FC = (): JSX.Element => {
-  return <section className={stylesProfile.page}>
-    <Statistics />
+  return (
+    <section className={stylesProfile.page}>
+      <Statistics />
+      <Card
+        size="big"
+        avatar="https://fs.znanio.ru/7ec5d2/d4/b5/750c4d0f7fe1f3fd9cba006fbfce6bc710.jpg"
+        name="Антонова Екатерина Владимировна"
+        appointment="Главный финансовый аналитик"
+      />
+      <Card
+        size="small"
+        // avatar="https://i.pinimg.com/originals/2f/b8/61/2fb861e3a0060ae2ce593877cff4edab.jpg"
+        name="Соколов Михаил 
+        Алексеевич"
+        appointment="Финансовый аналитик"
+      />
 
-
-            {/* <button
+      {/* <button
               type="button"
               onClick={() => {
                 const popupAssignment = "task";
@@ -23,7 +37,8 @@ const HomePage: FC = (): JSX.Element => {
             >
               Открыть popup task
             </button> */}
-  </section>;
+    </section>
+  );
 };
 
 export default HomePage;
