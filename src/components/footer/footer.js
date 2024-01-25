e13d11fb37c69906e2d9dac00cbaf7bc6066b2b1
Pavel Prokofev <kirgadan@yandex.ru>
2024-01-25 05:59:14 +0300

    поставил компонент link в footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import footerStyle from './footer.module.scss';
 import Unpacker from '../../ui/unpacker/unpacker.js'
 import LogoIcon from "../../ui/icons/logo";
+import Link from "../../ui/links/link";
 
 function Footer(props) {
   return (
@@ -11,13 +12,21 @@ function Footer(props) {
           props.footerLinkList.length ?
             props.footerLinkList.map((title) => <Unpacker key={title.id}>
               <li>
-                <a className={`${footerStyle[`footer__links`]}`} href={title.url} target='_blank' rel='noreferrer'>{title.title}</a>
+                <Link
+                  href={title.url}
+                  color='black'
+                  size='18'
+                  underline={false}
+                >
+                  {title.title}
+                </Link>
               </li>
-            </Unpacker>) : ''
+            </Unpacker>
+            ) : ''
         }
       </ul>
       <div className={`${footerStyle[`footer__logo`]}`}>
-        <LogoIcon width={'40'} height={'40'}/>
+        <LogoIcon width={'40'} height={'40'} />
       </div>
     </section>
   );
