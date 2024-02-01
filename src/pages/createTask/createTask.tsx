013fd1acf7ddd6ed7c5825a6c6e16f7c69a375da
Anastasiia-Niii <a.nistratova14@gmail.com>
2024-02-01 12:46:01 +0300

    feat: собрана страница создания задачи

--- src/pages/createTask/createTask.tsx
+++ src/pages/createTask/createTask.tsx
@@ -4,12 +4,15 @@ import styles from "./createTask.module.scss";
 
 import Link from "../../ui/links/link";
 import Button from "../../ui/buttons/button/button";
-
+import Input from "../../ui/inputs/input/input";
+import Textarea from "../../ui/textarea/textarea";
+import InputCalendar from "../../components/InputCalendar/InputCalendar";
 import Card from "../../components/card/card";
 
 import { isContainRoute } from "../../utils/breadcrumbs";
 
 const CreateTask: FC = (): JSX.Element => {
+  const [skills, setSkills] = useState("hard");
   const { state, pathname } = useLocation();
   const navigate = useNavigate();
   const url = window.location.href;
@@ -34,6 +37,12 @@ const CreateTask: FC = (): JSX.Element => {
     e.preventDefault();
     navigate("/employee-ipr/create-ipr", { replace: true, state: state.slice(0, 3)});
   };
+  function handleChange(e: any) {
+    console.log(e.target.value);
+  }
+  function handleSkillsChange(e: any) {
+    setSkills(e.target.value);
+  }
 
   return (
     <section className={styles.page}>
@@ -55,11 +64,51 @@ const CreateTask: FC = (): JSX.Element => {
           <Card
             size="small"
             // avatar="https://i.pinimg.com/originals/2f/b8/61/2fb861e3a0060ae2ce593877cff4edab.jpg"
-            name="Соколов Михаил 
-        Алексеевич"
+            name="Соколов Михаил Алексеевич"
             appointment="Финансовый аналитик"
           />
-          <p>Здесь будет создание задачи</p>
+          <section className={styles.listIpr}>
+            <div className={styles.nameIpr}>
+              <Input onChange={handleChange} />
+              <div className={styles.skillsIpr}>
+                <Button
+                  name="skill"
+                  value="hard"
+                  color={skills === "hard" ? "darkGrey" : "white"}
+                  width="98"
+                  heigth="40"
+                  onClick={handleSkillsChange}
+                >
+                  Hard skill
+                </Button>
+                <Button
+                  name="skill"
+                  value="soft"
+                  color={skills === "soft" ? "darkGrey" : "white"}
+                  width="98"
+                  heigth="40"
+                  onClick={handleSkillsChange}
+                >
+                  Soft Skill
+                </Button>
+              </div>
+            </div>
+            <Textarea
+              width="522px"
+              height="172px"
+              placeholder="Введите описание ИПР"
+            />
+            <div className={styles.dateIprWrapp}>
+              <div className={styles.dateIpr}>
+                <p className={styles.autorIpr}>Дата создания задачи</p>
+                <InputCalendar icon={true} />
+              </div>
+              <div className={styles.dateIpr}>
+                <p className={styles.autorIpr}>Дата закрытия задачи</p>
+                <InputCalendar icon={true} />
+              </div>
+            </div>
+          </section>
         </div>
         <div className={styles.wrapper__button}>
           <Button color="red" width="281" heigth="56" onClick={onClick}>
